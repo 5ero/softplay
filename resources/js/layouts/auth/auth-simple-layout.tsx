bdee2528d4a6e7418724958a1cf437aea8d6cb3a
Gareth <gareth71@gmail.com>
2025-11-20 07:14:50 +0000

    update logo on login

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -23,13 +23,13 @@ export default function AuthSimpleLayout({
                             href={home()}
                             className="flex flex-col items-center gap-2 font-medium"
                         >
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)]" />
+                            <div className="mb-1 flex h-32 w-32 items-center justify-center rounded-md">
+                                <AppLogoIcon className="size-24 fill-current text-[var(--foreground)]" />
                             </div>
                             <span className="sr-only">{title}</span>
                         </Link>
 
-                        <div className="space-y-2 text-center">
+                        <div className="space-y-1 text-center">
                             <h1 className="text-xl font-medium">{title}</h1>
                             <p className="text-center text-sm text-muted-foreground">
                                 {description}
