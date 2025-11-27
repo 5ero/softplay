c723fcae56bdbfdc0966fd1d862582e0788a7a15
Gareth <gareth71@gmail.com>
2025-11-27 10:20:51 +0000

    updated homepage

--- resources/js/components/app/hero.tsx
+++ resources/js/components/app/hero.tsx
@@ -6,14 +6,22 @@ const Hero = () => {
             <div className="flex flex-col items-center">
                 <div className="max-w-7xl mx-auto flex flex-col md:flex-row rounded-lg p-8">
                     <div className="w-full md:w-1/3">
-                        <h1 className="text-6xl font-bold">
-                            Soft play areas for <span className="text-orange-600">kids</span>
+                        <h1 className="text-5xl font-bold text-gray-700">
+                           The one stop shop for <span className='text-orange-600'>event planners</span> &  <span className='text-gray-400'>party people</span>
                         </h1>
                         <div className="md:hidden">
-                            <img src="/storage/img/displays/PhotoRoom_20231123_153629.png" alt="" />
+                            <img src="/storage/img/home/softplay-main-image.jpg" alt="" />
                         </div>
-                        <div className="prose text-xl leading-relaxed md:mt-4 text-gray-600">
-                            <span className="font-semibold text-2xl">Funtime Softplay</span> offer a vibrant and engaging environment designed to ignite the imagination and energy of young ones. These playgrounds are a haven of padded structures, ball pits, and bouncy castles, all carefully crafted to encourage exploration, social interaction, and physical activity.
+                        <div className="prose  leading-relaxed md:mt-4 text-gray-600">
+                            <div className='text-2xl mb-2'>
+                                <span className="font-semibold ">Funtime Softplay</span> From the humble beginnings of children’s soft play to a one stop event planning company. 
+                            </div>
+                            <ul className='text-lg'>
+                                <li>For weddings, For children’s parties inside and out, For birthday parties for all ages</li>
+                                <li>Soft play, Ride on carousels, Bouncy castles, Balloon houses, Bumper cars with track, Table and chairs</li>
+                                <li>For the night time disco! a fabulous 3D led infinity dance floor</li>
+                            </ul> 
+                             We offer Full party decor along side our parent company Love Balloons Bedfordshire.
                         </div>
 
                         <div className="order-last">
@@ -26,12 +34,12 @@ const Hero = () => {
                         </div>
                     </div>
                     <div 
-                        className="hidden md:block w-2/3 pl-8 h-[700px] ml-8" 
+                        className="hidden md:block w-1/2 pl-8 h-[700px] ml-8" 
                         style={{
                             backgroundPositionY: '-100px',
                             backgroundSize: 'cover',
                             backgroundRepeat: 'no-repeat',
-                            backgroundImage: "url('/storage/img/displays/PhotoRoom_20231123_153629.png')"
+                            backgroundImage: "url('/storage/img/home/softplay-main-image.jpg')"
                         }}
                     />
                 </div>
