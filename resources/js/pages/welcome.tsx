66aae999ba3f7c148a6c52c905b717d8da626aca
Gareth <gareth71@gmail.com>
2025-12-30 18:58:31 +0000

    updates

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -7,11 +7,13 @@ export default function Welcome() {
     return (
         <>
             <Head title="Welcome">
+                <meta name="description" content="Welcome to Fun Time Soft Play - the ultimate destination for children's soft play equipment and party packages. Discover our wide range of products and services designed to create unforgettable experiences for kids of all ages." />   
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link
                     href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
                     rel="stylesheet"
                 />
+                <link rel="canonical" href="https://funtimesoftplay.co.uk" />
             </Head>
             <div className="flex flex-col min-h-screen">
                 <Header />
