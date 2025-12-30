d2b7bbf6f88d3b3acb027f1d8b8cbb3051b095b5
Gareth <gareth71@gmail.com>
2025-12-30 16:35:31 +0000

    updates

--- resources/js/pages/gallery-item.tsx
+++ resources/js/pages/gallery-item.tsx
@@ -56,7 +56,7 @@ export default function GalleryItemView({ item }: Props) {
                     rel="stylesheet"
                 />
             </Head>
-            <div className="flex min-h-screen flex-col bg-amber-50">
+            <div className="flex min-h-screen flex-col bg-blue-50">
                 <Header />
                 <div className="container mx-auto px-4 py-12 md:mt-28">
                     {/* Back Button */}
