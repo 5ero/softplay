72683e5d214650c8a6f1cb1530fc468f58f51700
Gareth <gareth71@gmail.com>
2026-01-05 17:54:11 +0000

    updates

--- app/Models/GalleryItem.php
+++ app/Models/GalleryItem.php
@@ -5,11 +5,51 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class GalleryItem extends Model
 {
     /** @use HasFactory<\Database\Factories\GalleryItemFactory> */
     use HasFactory;
+
+    protected static function booted(): void
+    {
+        static::creating(function (GalleryItem $item) {
+            if (empty($item->slug) && ! empty($item->title)) {
+                $item->slug = static::generateUniqueSlug($item->title);
+            }
+        });
+
+        static::updating(function (GalleryItem $item) {
+            if ($item->isDirty('title') && empty($item->slug)) {
+                $item->slug = static::generateUniqueSlug($item->title, $item->id);
+            }
+        });
+    }
+
+    protected static function generateUniqueSlug(string $title, ?int $excludeId = null): string
+    {
+        $baseSlug = Str::slug($title);
+        $slug = $baseSlug;
+        $counter = 1;
+
+        $query = static::where('slug', $slug);
+        if ($excludeId !== null) {
+            $query->where('id', '!=', $excludeId);
+        }
+
+        while ($query->exists()) {
+            $slug = $baseSlug.'-'.$counter;
+            $counter++;
+
+            $query = static::where('slug', $slug);
+            if ($excludeId !== null) {
+                $query->where('id', '!=', $excludeId);
+            }
+        }
+
+        return $slug;
+    }
 
     protected $fillable = [
         'category_id',
