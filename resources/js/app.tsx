715d9979d563d6aa01e424bdd0782486ec850170
Gareth <gareth71@gmail.com>
2026-02-27 14:10:12 +0000

    update

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,7 +3,7 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Funtime Softplay';
@@ -16,9 +16,8 @@ createInertiaApp({
             import.meta.glob('./pages/**/*.tsx'),
         ),
     setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(
+        hydrateRoot(
+            el,
             <StrictMode>
                 <App {...props} />
             </StrictMode>,
